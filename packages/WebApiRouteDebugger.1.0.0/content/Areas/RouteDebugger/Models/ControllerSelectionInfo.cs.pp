b36b30161c76d12b42c9fa9511604294db6c9170
Clemens Vasters <clemens@vasters.com>
2013-09-02 20:15:45 +0200

    Cleanup

--- packages/WebApiRouteDebugger.1.0.0/content/Areas/RouteDebugger/Models/ControllerSelectionInfo.cs.pp
+++
@@ -1,9 +0,0 @@
-namespace $rootnamespace$.Areas.RouteDebugger.Models
-{
-    public class ControllerSelectionInfo
-    {
-        public string ControllerName { get; set; }
-
-        public string ControllerType { get; set; }
-    }
-}
